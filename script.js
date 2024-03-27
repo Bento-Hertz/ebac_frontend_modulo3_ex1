41391a0ce244af2b0b09ad75d06068825b18c044
Bento-Hertz <bentoluiz50@gmail.com>
2024-03-26 22:59:18 -0300

    chore: criando efeito de clique nos itens da lista com jquery

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+
+    const list = $('ul');
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+
+        const task = $('#task').val();
+        $(`<li>${task}</li>`).appendTo('ul');
+
+        $('#task').val('');
+    });
+
+    list.on('click', 'li', function(e) {
+        $(e.target).toggleClass('active');
+    });
+    
+})
