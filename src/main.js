a64e8083652ebdaefb1faaa7cfbc3becb9d2baf4
Bento-Hertz <bentoluiz50@gmail.com>
2024-04-23 14:27:14 -0300

    creating functions in typescript

---
+++ src/main.js
@@ -0,0 +1,8 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicacao(10, 11), '\n', saudacao('Bento'));
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+function multiplicacao(a: number, b:number): number {
+    return a * b;
+}
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+console.log(
+    multiplicacao(10, 11),
+    '\n',
+    saudacao('Bento')
+);
